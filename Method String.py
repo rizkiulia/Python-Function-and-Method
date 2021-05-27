8d44f78a1edb198c597103609e474172c34315c4
rizkiulia <rizkiulia96@gmail.com>
2021-05-27 17:31:23 +0700

    Create Method String.py

---
+++ Method String.py
@@ -0,0 +1,11 @@
+#Contoh Method String
+
+kalimat = 'Corona cepat selesai'
+
+# gunakan method untuk mengubah nilai kalimat menjadi uppercase semua kemudian tampilkan hasilnya
+print(kalimat.upper())
+
+
+
+# gunakan method untuk menghitung berapa huruf e di dalam kalimat
+print(kalimat.count('e'))
