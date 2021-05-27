e3d20452fa5a628e4ff95d3383889e54b14a0cc3
rizkiulia <rizkiulia96@gmail.com>
2021-05-27 17:20:15 +0700

    Create Built in Function.py

---
+++ Built in Function.py
@@ -0,0 +1,13 @@
+#Built-in Function adalah fungsi yang telah tersedia atau yang sudah disediakan oleh pemograman tersebut (fungsi bawaan)
+
+# 3 Contoh Built in Function 
+# max () ; min () ; float ()
+
+#Contoh penggunaan
+
+max ('hello world')
+
+min ('zebra')
+
+float ('39')
+
